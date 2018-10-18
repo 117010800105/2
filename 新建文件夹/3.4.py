eb272cfd2a2098e3ed57182bf2e6989bed029a2a
117010800105 <43087868+117010800105@users.noreply.github.com>
2018-10-19 07:42:33 +0800

    Update 3.4.py

--- 新建文件夹/3.4.py
+++ 新建文件夹/3.4.py
@@ -2,8 +2,9 @@
 import math
 TempStr=input("输入一个五位数：")
 if TempStr[0] in TempStr[-1]:
-    TempStr[1] in TempStr[-2]
-    print("yes")
+    if TempStr[1] in TempStr[-2]:
+        print("yes")
+    else:
+        print("no")
 else:
     print("no")
-
