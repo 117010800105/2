1b92d2898a06c1590425ae460fb549341efb7f8c
117010800105 <43087868+117010800105@users.noreply.github.com>
2018-10-19 09:40:47 +0800

    Update 3.6.py

--- 新建文件夹/3.6.py
+++ 新建文件夹/3.6.py
@@ -8,3 +8,17 @@ for i in range(scale+1):
     print("%{:^3.0f}[{}->{}]".format (c ,a , b))
     time.sleep(0.1)
 print("------执行结束------")
+
+
+mport time
+scale = 50
+t = time.clock()
+for i in range(scale + 1):
+    a = '.' * i
+    c = (i / scale) * 100
+    t -= time.clock()
+    print("\rStarting {:^3.0f}%[{}]{:.2f}s Done".format(c,a,-t), end='')
+    time.sleep(0.05)
+    
+    
+    
