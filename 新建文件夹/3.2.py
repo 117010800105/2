e47af5e20167d720b0468c7a445c280f99ca8796
117010800105 <43087868+117010800105@users.noreply.github.com>
2018-10-19 09:37:49 +0800

    Update 3.2.py

--- 新建文件夹/3.2.py
+++ 新建文件夹/3.2.py
@@ -8,3 +8,10 @@ for i in range(365):
         dayup = dayup * (1+dayfactor)
 print("能力值:{:.2f}.".format(dayup))
     
+dayup, dayfactor = 1.0, 0.01
+for i in range(1,366):
+    if i % 50 in [0,1,2,3]:
+        dayup = dayup
+    elif i % 7 in [0,6,5,4]:
+        dayup = dayup * (1 + dayfactor)
+print("连续学习3天能力值不变，从第4天至第7天每天能力增长为前一天1%的力量: {:.2f}.".format(dayup))
