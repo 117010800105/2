ca8dffffa1b48b88a94ff05cbb6c9f51aeb41e59
117010800105 <43087868+117010800105@users.noreply.github.com>
2018-10-19 09:39:59 +0800

    Update 3.3.py

--- 新建文件夹/3.3.py
+++ 新建文件夹/3.3.py
@@ -22,6 +22,13 @@ for i in range(1,366):  #0-6 7天  1-7 7
     if i % 10 in [4,5,6,7]:
         dayup = dayup * (1 + dayfactor)
     print("{}：天的能力{}".format(i+1,dayup))
+    
+    
+    dayup, dayfactor = 1.0, 0.01
+for i in range(1,366):  #0-6 7天 1-7 7
+    if i % 15 in [4,5,6,7,8,9,10,11,12]:
+ dayup = dayup * (1 + dayfactor)
+    print("{}：能力{}".format(i+1,dayup))
 
 
 
