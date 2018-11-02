dbdd41492264f48db249a31920edfcee23c26a11
117010800105 <43087868+117010800105@users.noreply.github.com>
2018-11-02 08:48:35 +0800

    Update 4.1.py

--- 4/4.1.py
+++ 4/4.1.py
@@ -35,6 +35,24 @@ for c in s:
 print('有{0}个英文字符，{1}个数字，{2}个空格和{3}个其它字符'.format(n_alphabet, n_number, n_space, n_other))
 
 
+a=0
+b=0
+c=0
+d=0
+e=input("")
+for i in range(len(e)):
+    if e[i]==" ":
+        a=a+1
+    elif "A" <e[i] <= "Z" or "a" <e[i]<= "z":
+        b=b+1
+    elif "0"<=e[i]<="9":
+        c=c+1
+    else:
+        d=d+1
+print("{}{}{}{}".format(a,b,c,d))
+
+
+
 
 a=eval(input(""))
 b=eval(input(""))
@@ -48,3 +66,13 @@ while(r!=0):
     r=m%n
 print("{}".format(n))
 print("{}".format(a*b/n))
+
+
+n1, n2 = eval(input('请输入两个整数，中间用逗号隔开：'))
+x, y = n1, n2
+r = x % y
+while r > 0:
+    x,y = y,r
+    r = x % y
+
+print('{}和{}最大公约数是：{}， 最小公倍数是：{}'.format(n1, n2, y, n1 * n2 / y))
