f53c86791ecc56096fcac5ebf0a1863d1fb21c37
117010800105 <43087868+117010800105@users.noreply.github.com>
2018-11-02 08:28:27 +0800

    Update 4.1.py

--- 4/4.1.py
+++ 4/4.1.py
@@ -14,3 +14,37 @@ while True:
         print("中")
         print(format(count))
         break
+        
+        
+        
+n_alphabet = 0
+n_alphabet = 0
+n_number = 0
+n_space = 0
+n_other = 0
+s = input('请输入一行字符')
+for c in s:
+    if 'A' <= c <= 'Z' or 'a' <= c <= 'z':
+        n_alphabet += 1
+    elif '0' <= c <= '9':
+        n_number += 1
+    elif c == ' ':
+        n_space += 1
+    else:
+        n_other += 1
+print('有{0}个英文字符，{1}个数字，{2}个空格和{3}个其它字符'.format(n_alphabet, n_number, n_space, n_other))
+
+
+
+a=eval(input(""))
+b=eval(input(""))
+if a<b:
+    m,n=b,a
+else:
+    m,n=a,b
+r=m%n
+while(r!=0):
+    m,n=n,r
+    r=m%n
+print("{}".format(n))
+print("{}".format(a*b/n))
