af28688b8a5b5169621ec86f95a9a22555c15013
117010800105 <43087868+117010800105@users.noreply.github.com>
2018-12-25 19:37:43 +0800

    Update 5.4.py

--- 5/5.4.py
+++ 5/5.4.py
@@ -7,3 +7,40 @@ def multi(*a): #发表示a可变
     return t
 
 print( multi(1,2,3,4,5,6,7,8,9,10))
+import requests
+from bs4 import BeautifulSoup
+allUniv=[]
+def getHTMLText(url):
+    try:
+        r=requests.get(url,timeout=30)
+        r.raise_for_status()
+        r.encoding='utf-8'
+        return r.text
+    except:
+        return ""
+def fillUnivList(soup):
+    date=soup.find_all('tr')
+    for tr in date:
+        ltd=tr.find_all('td')
+        if len(ltd)==0:
+            continue
+        singleUniv = []
+        for td in ltd:
+            singleUniv.append(td.string)
+
+        allUniv.append(singleUniv)
+
+def printUnivList(province):
+    print("{1:^2}{2:{0}^10}{3:{0}^6}{4:{0}^4}{5:{0}^10}".format(chr(12288),"排名","学校名称","省市","总分","培养规模"))
+    for u in allUniv:
+        if province in u[2]:
+            print("{1:^2}{2:{0}^10}{3:{0}^6}{4:{0}^4}{5:{0}^10}".format(chr(12288),u[0],u[1],u[2],u[3],u[4]))
+
+def main(p):
+    url='http://www.zuihaodaxue.cn/zuihaodaxuepaiming2018.html'
+    html=getHTMLText(url)
+    soup=BeautifulSoup(html,'html.parser')
+    fillUnivList(soup)
+    printUnivList(p)
+
+main('江西')
